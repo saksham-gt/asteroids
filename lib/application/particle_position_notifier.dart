b4a6cea3aba43164309e8103883a6cbef8847af9
Saksham Gupta <55953830+glitchpop-frenzy@users.noreply.github.com>
2025-03-22 10:54:25 +0530

    feat: spawn asteroids evert 200ms, handle collision b/w asteroids

--- lib/application/particle_position_notifier.dart
+++ lib/application/particle_position_notifier.dart
@@ -13,33 +13,43 @@ class ParticlePositionNotifier extends ValueNotifier<List<Particle>> {
   final BuildContext context;
 
   late ParticleInterface _particleRepository;
-  late Timer timer;
+
+  ParticleInterface get repository => _particleRepository;
+
+  late Timer spawnTimer;
 
   ParticlePositionNotifier(this.context, super.value) {
     _particleRepository = ParticleRepositoryImpl(context);
-    Timer.periodic(Duration(milliseconds: 200), (timer) {
-      _particleRepository.randomlySpawnParticles();
+
+    spawnTimer = Timer.periodic(Duration(milliseconds: 200), (timer) {
+      // Spawn new obstacles every 100 millisecs. and notify
+      _particleRepository.spawnParticles();
+      value = _particleRepository.particles;
     });
   }
 
   @override
   void dispose() {
     super.dispose();
-    timer.cancel();
+    spawnTimer.cancel();
   }
 
   void updateParticles() {
     _particleRepository.eliminateParticlesOutOfViewPort();
+
+    if (_particleRepository.particles.isEmpty) {
+      _particleRepository.spawnParticles();
+      value = _particleRepository.particles;
+    }
+
+    _particleRepository.detectCollisionBetweenObstacles();
+
     for (Particle particle in _particleRepository.particles) {
+      /// Since this method is triggered by controller whenever a certain time is elapsed and triggered by ticker (60 fps),
+      /// We check exactly the animation progress by controller value and move the [Particle] accordingly.
       particle.position += particle.velocity;
     }
     value = _particleRepository.particles;
     notifyListeners();
   }
-
-  void checkCollisionBetweenObstacles() {
-    
-  }
-
-  void checkCollisionBetweenPlayerAndObstacles() {}
 }
